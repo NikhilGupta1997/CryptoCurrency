0a7aaf3958531ee2ea6364328dba3e0bcaafa071
NikhilGupta1997 <nikhilgupta1997@gmail.com>
2018-01-20 15:32:31 +0530

    Part 3

--- cryptocurrency.cpp
+++ cryptocurrency.cpp
@@ -7,10 +7,7 @@ using namespace std;
 // Global Variables
 int N = 10; //Number of peers
 float txn_mean = 100.0; // Mean of exponential transaction distribution function
-
-// Random Generation Machine
-random_device rd;
-mt19937 gen(rd());
+float z = 0.5; // Probability of a fast node
 	
 // Classes
 class peer {
@@ -18,7 +15,7 @@ class peer {
 	int ID;
 	float amount;
 	bool type;
-	exponential_distribution<> dist(txn_mean);
+	
   public:
 	vector<int> conn;
 
@@ -55,14 +52,10 @@ class peer {
 	void add_conn(int id) { // Add peer to list of connected nodes
 		conn.push_back(id);
 	}
-
-	float get_txn_time() { // inter-arrival times between transactions
-		return dist(gen)
-	}
 };
 
-class tnx {
-  private:
+struct tnx {
+  public:
   	int send_id;
   	int recv_id;
   	float amount;
@@ -74,6 +67,62 @@ class tnx {
   	}
 };
 
+// Maintains the cryptocurrency network
+class network {
+  private:
+  	vector<peer*> nodelist;
+	vector<peer*> *adjlist;
+
+  public:
+  	network() {
+  		adjlist = new vector<peer*>[N];
+  	}
+
+  	peer* findnode(int id) {
+		int nsize = nodelist.size();
+		for (int i = 0; i < nsize; i++) {
+			if (nodelist[i]->get_id() == id)
+				return nodelist[i];
+		}
+		return NULL; ///// return NULL if node does not exist
+	}
+
+	void addnode(int id, bool type) {
+		if (findnode(id) == NULL) {
+			peer* newnode = new peer(id, type);
+			nodelist.push_back(newnode);
+		}
+	}
+
+	void addconn(int u, int v) {
+		adjlist[u].push_back(findnode(v));
+	}
+
+	bool findconn(int u, int v) {
+		int usize = adjlist[u].size();
+		for (int i = 0; i < usize; i++) {
+			if (adjlist[u][i]->get_id() == v)
+				return true;
+		}
+		return false;
+	}
+
+	bool validate(tnx transaction) {
+		int send_id = transaction.send_id;
+		int txn_amount = transaction.amount;
+		peer* send_node = findnode(send_id);
+		if (send_node->get_amount() < txn_amount) 
+			return false;
+		else
+			return true;
+	}
+};
+
+
 int main() {
+	random_device rd;
+	mt19937 gen(rd());
+	exponential_distribution<> exp_dist(10);
+	uniform_real_distribution<> uni_dist(0, 1);
 	return 0;
 }
