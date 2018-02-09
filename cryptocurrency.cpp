8d08e5964a89b19a0616b0446213ba81bfb81761
Ayush Bhardwaj <ayushbhardwaj@Ayushs-MacBook-Pro.local>
2018-02-09 19:01:47 +0530

    Transaction structure partially added

--- cryptocurrency.cpp
+++ cryptocurrency.cpp
@@ -4,7 +4,8 @@
 #include<deque>
 #include<map>
 #include<math.h>
-
+#include<time.h>
+#include <limits>
 using namespace std;
 
 // Global Variables
@@ -15,6 +16,9 @@ float ff = 0.5;
 float fs = 0.25;
 float ss = 0.1;
 
+
+int txn_counter = 0;
+int block_counter = 1; // purpose : maintain unique block ids
 // Randomizor Functions
 float exp_dist(float mean) {
 	random_device rd;
@@ -29,7 +33,149 @@ float uni_dist(float start, float end) {
 	uniform_real_distribution<> dist(start, end);
 	return dist(gen);
 }
+
+struct tnx {
+	int id;
+  	int send_id;
+  	int recv_id;
+  	float amount;
+  	double time;
+
+  	tnx(int txn_id, int id1, int id2, float val, double time) { // Create a new transaction
+  		id 		= txn_id;
+  		send_id = id1;
+  		recv_id = id2;
+  		amount  = val;
+  		time = this->time;
+  	}
+};
+
+struct block
+{
+	int prevblockID;
+	int blockID;
+	double time_arrival;
+	// I should have a list of transactions as well or make a map for every block and transactions in it
+};
+struct node
+{
+	block *blk;
+	vector<node *> nextBlocks;		
+};	
+struct ans_long_chain
+{
+	node *last_node;
+	double time;
+	int length;
+	ans_long_chain(double time, int length)
+	{
+		this->time = time;
+		this->length = length;
+	}
+	ans_long_chain(node *last_block, double time, int length)
+	{
+		this->last_node = last_block;
+		this->time = time;
+		this->length = length;
+	}
+};	
+class blockchain
+{
 	
+	node *root;
+
+	node* find_block(node *start, int block_id)
+	{
+		if(start->blk->blockID == block_id)
+		{
+			return start;
+		}
+		// if(start->nextBlocks.size() == 0)
+		// 	return NULL;
+		for(auto child : start->nextBlocks)
+		{
+			node *ans = find_block(child, block_id);
+			if(ans != NULL)
+				return ans;
+		}
+		return NULL;
+	}
+
+	ans_long_chain getLongestChain(node *start, int length)
+	{
+		if(start->nextBlocks.size() == 0)
+		{
+			ans_long_chain ans(start->blk->time_arrival, length+1);
+			ans.last_node = start;
+			return ans;
+		}
+		node* lastNode;
+		int max_length = -1;
+		double min_time = std::numeric_limits<double>::max();
+		for(auto child : start->nextBlocks)
+		{
+			ans_long_chain child_ans = getLongestChain(child, length+1);
+			if((child_ans.length > max_length) || ((child_ans.length == max_length) && (child_ans.time < min_time)))
+			{
+				max_length = child_ans.length;
+				min_time = child_ans.time;
+				lastNode = child_ans.last_node;
+			}
+		}
+		ans_long_chain final_ans(lastNode, min_time ,max_length);
+		return final_ans;
+	}
+public:
+
+	blockchain()
+	{
+		root = new node();
+		root->blk->blockID = 0;	
+	}
+
+	void add_block(int prevblockID, int blockID, double time_arrival)
+	{
+		node *lastNode = find_block(root, prevblockID);
+		if(lastNode == NULL)
+		{
+			cout<<"Error"<<endl;
+			return;
+		}
+		node *newNode = new node();
+		newNode->blk->prevblockID = prevblockID;
+		newNode->blk->blockID = blockID;
+		newNode->blk->time_arrival = time_arrival;
+		lastNode->nextBlocks.push_back(newNode);
+		// TODO : Manage list of transactions coming, adjust time based on latency..
+	}
+
+	void generate_node(double time)
+	{
+		node *current = new node();
+		current->blk = new block();
+		current->blk->blockID = block_counter;
+		node *longestChain = (getLongestChain(root, 0).last_node);
+		current->blk->prevblockID = longestChain->blk->blockID;
+		current->blk->time_arrival = time;
+		longestChain->nextBlocks.push_back(current);
+		// we get the previous blockid
+		block_counter++;
+		// Now we need to broadcast this also 
+	}
+};	
+
+void sorted_add(tnx trans, vector <tnx> &globalQueueTnx)
+{
+	if(globalQueueTnx.size() == 0)
+		globalQueueTnx.push_back(trans);
+	int i = 0;
+	for( ; i < globalQueueTnx.size(); i++)
+	{
+		if(globalQueueTnx[i].time > trans.time)
+			break;
+	}
+	globalQueueTnx.insert(globalQueueTnx.begin()+i, trans);
+}
 // Classes
 class peer {
   private:
@@ -38,8 +184,8 @@ class peer {
 	bool type;
 	int activation;
 	int connected;
-	vector<int> txns;
-	
+	// vector<int> txns;
+	vector <tnx> globalQueueTnx;
   public:
 
 	peer(int id, bool speed, int active) {
@@ -48,6 +194,22 @@ class peer {
 		type = speed;
 		activation = active;
 		connected = 0;
+	}
+
+
+	void generate_transaction(double time) // for intial time would be zero
+	{
+		float pay_amt = amount * (static_cast <float> (rand()) / static_cast <float> (RAND_MAX));
+		int rec = rand() % N;
+		while(rec != ID)
+		{
+			rec = rand() % N;
+		}
+		double next_time = time + exp_dist(txn_mean);
+		tnx trans(++txn_counter, ID, rec, pay_amt, next_time);
+		sorted_add(trans, globalQueueTnx);
+		// globalQueueTnx.add(txn_counter);
+		// TODO : I have to broadcast it as well (Important)
 	}
 
 	int get_id() { // Get node id
@@ -85,38 +247,28 @@ class peer {
 		connected += 1;
 	}
 
-	void add_txn(int id) {
-		txns.push_back(id);
+	void add_txn(tnx trans) {
+		// need to change the time
+		globalQueueTnx.push_back(trans);
 	}
 
 	bool txn_exists(int id) {
-		if (find(txns.begin(), txns.end(), id) != txns.end())
-			return true;
-		else
-			return false;
+		for(auto it : globalQueueTnx)
+			if(it.id == id)
+				return true;
+		return false;	
+		// if (find(globalQueueTnx.begin(), txns.end(), id) != txns.end())
+		// 	return true;
+		// else
+		// 	return false;
 	}
 };
 
-struct tnx {
-  private:
-  	int id;
-  public:
-  	int send_id;
-  	int recv_id;
-  	float amount;
-
-  	tnx(int txn_id, int id1, int id2, float val) { // Create a new transaction
-  		id = txn_id;
-  		send_id = id1;
-  		recv_id = id2;
-  		amount = val;
-  	}
-};
 
 // Maintains the cryptocurrency network
 class network {
   private:
-  	vector<peer*> nodelist;
+  	
 	map<int, vector< pair< peer*, float > > > adjlist;
 	deque<peer*> unactive;
 
@@ -132,6 +284,7 @@ class network {
 
   public:
 
+  	vector<peer*> nodelist;
   	peer* findnode(int id) {
 		int nsize = nodelist.size();
 		for (int i = 0; i < nsize; i++) {
@@ -263,23 +416,24 @@ class network {
 		return total_latency;
 	}
 
-	void broadcast(int id, peer* recv_node, int send_id) {
-		if (recv_node->txn_exists(id))
+	void broadcast(tnx &trans, peer* recv_node, int send_id) {
+		if (recv_node->txn_exists(trans.id))
 			return;
 		else {
-			recv_node->add_txn(id);
+			recv_node->add_txn(trans);
 			for( const auto& peerlist : adjlist[recv_node->get_id()] ) {
 				if(peerlist.first->get_id() != send_id)
-					broadcast(id, peerlist.first, recv_node->get_id());
+					broadcast(trans, peerlist.first, recv_node->get_id());
 			}
 		}
 	}
 };
 
 int main() {
+
 	// Create P2P network for cryptocurrency
 	network mycoin;
-	
+
 	// Add peers the network
 	for (int i = 0; i < N; i++) {
 		float node_type_prob = uni_dist(0,1);
@@ -297,11 +451,16 @@ int main() {
 	// Connect peers in the network
 	mycoin.connect_graph();
 
-	mycoin.print_graph();
+	for(int i = 0; i < N; i++)
+	{
+		mycoin.nodelist[i]->generate_transaction(0.0);
+	}
 
-	cout << mycoin.get_latency(mycoin.findnode(0), mycoin.findnode(2), 1);
+	// mycoin.print_graph();
 
-	mycoin.broadcast(10, mycoin.findnode(0), 1);
+	// cout << mycoin.get_latency(mycoin.findnode(0), mycoin.findnode(2), 1);
+
+	// mycoin.broadcast(10, mycoin.findnode(0), 1);
 
 	cout<<"\nend"<<endl;
 	return 0;
